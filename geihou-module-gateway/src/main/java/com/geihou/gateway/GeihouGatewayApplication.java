15b3b551e23a07c9dec2c82d05e9f081df51f3ab
leven <104824531@qq.com>
2026-07-08 11:33:53 +0800

    feat: add geihou gateway minimal boot skeleton

---
+++ geihou-module-gateway/src/main/java/com/geihou/gateway/GeihouGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.geihou.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeihouGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeihouGatewayApplication.class, args);
+    }
+}
